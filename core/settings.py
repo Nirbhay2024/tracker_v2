a615134ca4bb59c7d2e1283f51ea6f737e38fb87
Nirbhay <nirbhayshukla2024@gmail.com>
2026-01-04 09:13:37 +0530

    Latest push

--- core/settings.py
+++ core/settings.py
@@ -45,13 +45,13 @@ CSRF_TRUSTED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'tracker',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tracker',
     'cloudinary_storage',
     'cloudinary',
 ]
